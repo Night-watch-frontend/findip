0340a6ba25b22ef273614f573754208ff9d1ad88
Sergey <murderer83@gmail.com>
2024-10-09 02:05:32 +0300

    added validate ip

--- src/index.js
+++ src/index.js
@@ -1,3 +1,5 @@
+import { validateIP } from "./helpers";
+
 const ipInput = document.querySelector(".search-bar__input");
 const btn = document.querySelector(".search-bar__btn");
 
@@ -5,15 +7,16 @@ btn.addEventListener("click", getData);
 ipInput.addEventListener("keydown", handleKey);
 
 function getData() {
-  fetch(
-    `https://geo.ipify.org/api/v2/country,city?apiKey=at_IdEHDaDCryRksyyLgC8nbMLhVTDXb&ipAddress=${ipInput.value}`
-  )
-    .then((response) => response.json())
-    .then(console.log);
+  if (validateIP(ipInput.value)) {
+    fetch(
+      `https://geo.ipify.org/api/v2/country,city?apiKey=at_IdEHDaDCryRksyyLgC8nbMLhVTDXb&ipAddress=${ipInput.value}`
+    )
+      .then((response) => response.json())
+      .then(console.log);
+  }
 }
 
 function handleKey(e) {
-  console.log(e);
   if (e.key === "Enter") {
     getData();
   }
