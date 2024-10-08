d1ba1c28097a6abce9bca6549e8967f8151ede1d
Sergey <murderer83@gmail.com>
2024-10-09 01:51:37 +0300

    added ip

--- src/index.js
+++ src/index.js
@@ -1,1 +1,20 @@
+const ipInput = document.querySelector(".search-bar__input");
+const btn = document.querySelector(".search-bar__btn");
 
+btn.addEventListener("click", getData);
+ipInput.addEventListener("keydown", handleKey);
+
+function getData() {
+  fetch(
+    `https://geo.ipify.org/api/v2/country,city?apiKey=at_IdEHDaDCryRksyyLgC8nbMLhVTDXb&ipAddress=${ipInput.value}`
+  )
+    .then((response) => response.json())
+    .then(console.log);
+}
+
+function handleKey(e) {
+  console.log(e);
+  if (e.key === "Enter") {
+    getData();
+  }
+}
